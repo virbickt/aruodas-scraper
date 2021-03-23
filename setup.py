fa38419a86577e0b45fc856bc08a433c212eb03e
virbickt <63246400+virbickt@users.noreply.github.com>
2021-03-23 12:15:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,6 @@ from distutils.core import setup
 setup(
     name='aruodas_scraper',
     version='1.0',
-    packages=['aruodas-scraper'],
-    url='https://github.com/virbickt/aruodas-scraper',
-    license='MIT License',
-    author='Teofilius Virbickas',
-    author_email='tvirbickas@gmail.com',
-    description='Webscraper designed to scrape rent listings on aruodas.lt',
-    install_requires=[
-        'requests',
-        'bs4',
-        'pandas',
-        'fake_useragent'
-    ]
+    py_modules=['aruodas_scraper'],
+    package_dir={"": "aruodas_scraper"}
 )
